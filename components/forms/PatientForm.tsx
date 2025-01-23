779b6de13636030e7257b3ec8ab185f12f9c691d
Santraj Kumar <santrajmaurya1992@gmail.com>
2025-01-23 16:59:29 +0530

    added form validation

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,58 +1,87 @@
 "use client"
 
+import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../ui/CustomFormField"
+import SubmitButton from "../ui/SubmitButton"
+import { UserFormValidation } from "@/lib/validatons"
+import { useRouter } from "next/navigation"
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
+
+export enum FormFieldType {
+    INPUT = 'input',
+    TEXTAREA = 'textarea',
+    PHONE_INPUT = 'phoneInput',
+    CHECKBOX = 'checkbox',
+    DATE_PICKER = 'datePicker',
+    SELECT = 'select',
+    RADIO = 'radio'
+}
+
+
 
 const PatientForm = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const router = useRouter()
+    const [isLoading, setIsLoading] = useState(false)
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
         defaultValues: {
-            username: "",
+            name: "",
+            email: "",
+            phone: ""
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values)
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true)
+        try {
+            // const userData = { name, email, phone };
+            // const user = await createUser(userData)
+            // if (user) router.push(`/patients/${user.$id}/register`)
+
+        } catch (error) {
+            console.log(error)
+
+        }
     }
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+                <section className="mb-12 space-y-4">
+                    <h1 className="header">Hi There 👋</h1>
+                    <p className="text-dark-700">Schedule your first appointment</p>
+                </section>
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
                     control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
+                    name="name"
+                    label="Full Name"
+                    placeHolder="John Doe"
+                    iconSrc="/assets/icons/user.svg"
+                    iconAlt="user"
                 />
-                <Button type="submit">Submit</Button>
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeHolder="johndoe@gmail.com"
+                    iconSrc="/assets/icons/email.svg"
+                    iconAlt="email"
+                />
+
+                <CustomFormField
+                    fieldType={FormFieldType.PHONE_INPUT}
+                    control={form.control}
+                    name="phone"
+                    label="Phone"
+                    placeHolder="(555) 123-4587"
+                />
+                <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
             </form>
         </Form>
     )
